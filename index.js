3bef14d42a009bd8c4f13d753b506f9326095167
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-11-18 02:15:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,4 @@ function happyHolidays() {
  return "Happy holidays!" }
 
  function happyHolidaysTo(name) {
- var name
- return "Happy holidays, " + name! }
+ return "Happy holidays, " + name + "!" }
